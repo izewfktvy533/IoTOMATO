4c5eccbbd5bf03ea4ddcb62892a05b07a63d5224
Taichi Soma <izewfktvy533zjmn@gmail.com>
2019-10-03 12:17:45 +0900

    MQTT通信処理の変更
    通信処理のコードをコメントアウトした。

--- gateway/gateway.py
+++ gateway/gateway.py
@@ -8,35 +8,16 @@ import time
 import os
 
 from xbee import ZigBee
-import paho.mqtt.client as mqtt
+#import paho.mqtt.client as mqtt
 
 
 PORT = '/dev/ttyUSB0'
 BAND_RATE = 9600
-MQTT_BROKER_ADDR = '172.29.156.95'
+MQTT_BROKER_ADDR = '192.168.3.10'
 MQTT_BROKER_PORT = 1883
 MAIN_TOPIC = "IoTOMATO/sensing_part/"
 DIRECTORY_NAME = "/home/pi/WorkSpace/IoTOMATO/gateway/data/sensing_part/"
 
-
-serial_port = serial.Serial(PORT, BAND_RATE)
-
-mqtt_client = mqtt.Client(protocol=mqtt.MQTTv31)
-mqtt_client.connect(host=MQTT_BROKER_ADDR, port=MQTT_BROKER_PORT, keepalive=0)
-
-xbee = ZigBee(serial_port, escaped=True, callback=handle_xbee)
-
-
-try:
-    while True:
-        time.sleep(0.000000001)
-
-except KeyboardInterrupt:
-    mqtt_client.disconnect()
-    xbee.halt()
-    serial_port.close()
-    exit(1)
- 
 
 def store_data(directory_name_str, file_name_str, payload_dit):
     os.chdir(DIRECTORY_NAME)
@@ -62,6 +43,7 @@ def send_mqtt_broker(sub_topic_str, payload_dit):
 
 
 def handle_xbee(xbee_packet):
+    print(xbee_packet)
     timestamp_datetime = datetime.now()
     timestamp_str = timestamp_datetime.strftime("%Y-%m-%dT%H:%M:%S")
     file_name_str = timestamp_datetime.strftime("%Y-%m-%d") + ".json"
@@ -74,22 +56,41 @@ def handle_xbee(xbee_packet):
         payload_dit.update({'timestamp' : timestamp_str})
 
         directory_name_str = sub_topic_str
+
+        print(payload_dit)
         
-        thread_send_mqtt_broker = threading.Thread(target=send_mqtt_broker, args=(sub_topic_str, payload_dit))
+        #thread_send_mqtt_broker = threading.Thread(target=send_mqtt_broker, args=(sub_topic_str, payload_dit))
         thread_store_data = threading.Thread(target=store_data, args=(directory_name_str, file_name_str, payload_dit))
         
-        thread_send_mqtt_broker.start()
+        #thread_send_mqtt_broker.start()
         thread_store_data.start()
 
-        thread_send_mqtt_broker.json()
-        thread_store_data.json()
-
-    except UnicodeDecodeError:
-        pass
+        #thread_send_mqtt_broker.join()
+        thread_store_data.join()
 
     except UnicodeDecodeError: 
         pass
 
     except ValueError:
         pass
+
+
+if __name__ == '__main__':
+    serial_port = serial.Serial(PORT, BAND_RATE)
+
+    #mqtt_client = mqtt.Client(protocol=mqtt.MQTTv31)
+    #mqtt_client.connect(host=MQTT_BROKER_ADDR, port=MQTT_BROKER_PORT, keepalive=0)
+
+    xbee = ZigBee(serial_port, escaped=True, callback=handle_xbee)
+
+
+    try:
+        while True:
+            time.sleep(0.000000001)
+
+    except KeyboardInterrupt:
+        mqtt_client.disconnect()
+        xbee.halt()
+        serial_port.close()
+        exit(1)
 
