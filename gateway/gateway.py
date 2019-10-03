a966a2f4cae588a2be6b55a224d5a530e7829ad1
Taichi Soma <izewfktvy533zjmn@gmail.com>
2019-10-03 17:23:00 +0900

    fix gateway.py

--- gateway/gateway.py
+++ gateway/gateway.py
@@ -1,26 +1,49 @@
-from xbee import ZigBee
-import paho.mqtt.client as mqtt
-import serial
-import sys
-import time
-import threading
-from datetime import datetime
-import os
-import json
+#!/usr/bin/env python3
+
 import ast
+from datetime import datetime
+import json
+import serial
+import time
+import os
+
+from xbee import ZigBee
+#import paho.mqtt.client as mqtt
 
 
 PORT = '/dev/ttyUSB0'
 BAND_RATE = 9600
-SERIAL_PORT = serial.Serial(PORT, BAND_RATE)
-MQTT_BROKER_ADDR = '172.29.156.95'
+MQTT_BROKER_ADDR = '192.168.3.10'
 MQTT_BROKER_PORT = 1883
-MAIN_TOPIC = "inouelab/sensing_part/"
+MAIN_TOPIC = "IoTOMATO/sensing_part/"
 DIRECTORY_NAME = "/home/pi/WorkSpace/IoTOMATO/gateway/data/sensing_part/"
 
 
+def store_data(directory_name_str, file_name_str, payload_dit):
+    os.chdir(DIRECTORY_NAME)
+    cpath = os.path.abspath("./")
 
-def handleXBee(xbee_packet):
+    try:
+        os.chdir(directory_name_str)
+    
+    except FileNotFoundError:
+        os.mkdir(directory_name_str)
+        os.chdri(directory_name_str)
+
+    with open(file_name_str, 'a') as fp:
+        json.dump(payload_dit, fp)
+        fp.write("\n")
+    
+    os.chdir(cpath)
+
+
+def send_mqtt_broker(sub_topic_str, payload_dit):
+    mqtt_packet = json.dumps(payload_dit)
+    mqtt_client.publish(MAIN_TOPIC + sub_topic_str, mqtt_packet, qos=0)
+
+
+def handle_xbee(xbee_packet):
+    print(xbee_packet)
     timestamp_datetime = datetime.now()
     timestamp_str = timestamp_datetime.strftime("%Y-%m-%dT%H:%M:%S")
     file_name_str = timestamp_datetime.strftime("%Y-%m-%d") + ".json"
@@ -31,48 +54,43 @@ def handleXBee(xbee_packet):
         payload_dit.update(data_dit)
         sub_topic_str = list(payload_dit.keys())[0]
         payload_dit.update({'timestamp' : timestamp_str})
+
         directory_name_str = sub_topic_str
+
+        print(payload_dit)
         
         #thread_send_mqtt_broker = threading.Thread(target=send_mqtt_broker, args=(sub_topic_str, payload_dit))
         thread_store_data = threading.Thread(target=store_data, args=(directory_name_str, file_name_str, payload_dit))
-
-        mqtt_packet = json.dumps(payload_dit)
-        mqtt_client.publish(MAIN_TOPIC + sub_topic_str, mqtt_packet, qos=0)
-
-        cpath = os.path.abspath("./")
-
-        try:
-            os.chdir(sub_topic_str)
         
-        except FileNotFoundError:
-            os.mkdir(sub_topic_str)
-            os.chdri(sub_topic_str)
+        #thread_send_mqtt_broker.start()
+        thread_store_data.start()
 
-        with open(file_name_str, 'a') as fp:
-            json.dump(payload_dit, fp)
-            fp.write("\n")
-        
-        os.chdir(cpath)
+        #thread_send_mqtt_broker.join()
+        thread_store_data.join()
 
     except UnicodeDecodeError: 
-        None
+        pass
 
     except ValueError:
-        None
+        pass
 
 
 if __name__ == '__main__':
-    os.chdir(DIRECTORY_NAME)
-    xbee = ZigBee(SERIAL_PORT, escaped=True, callback=handleXBee)
-    mqtt_client = mqtt.Client(protocol=mqtt.MQTTv31)
-    mqtt_client.connect(host=MQTT_BROKER_ADDR, port=MQTT_BROKER_PORT, keepalive=0)
+    serial_port = serial.Serial(PORT, BAND_RATE)
+
+    #mqtt_client = mqtt.Client(protocol=mqtt.MQTTv31)
+    #mqtt_client.connect(host=MQTT_BROKER_ADDR, port=MQTT_BROKER_PORT, keepalive=0)
+
+    xbee = ZigBee(serial_port, escaped=True, callback=handle_xbee)
+
 
     try:
         while True:
-            time.sleep(0.000001)
+            time.sleep(0.000000001)
 
     except KeyboardInterrupt:
-        #mqtt_client.disconnect()
+        mqtt_client.disconnect()
         xbee.halt()
-        SERIAL_PORT.close()
-        sys.exit(1)
+        serial_port.close()
+        exit(1)
+
