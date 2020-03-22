460d0390ec57eb31e9d48ea81febf930544a33c7
Taichi Soma <izewfktvy533zjmn@gmail.com>
2020-03-22 11:07:17 +0900

    ゲートウェイ部の刷新
    ゲートウェイプログラムを変更しました。

--- gateway/gateway.py
+++ gateway/gateway.py
@@ -17,13 +17,13 @@ BROKER_PORT = 1883
 AWS_HOST   = "a2jbwrz4hgj7v1-ats.iot.ap-northeast-1.amazonaws.com"
 AWS_PORT   = 8883
 CA_PATH    = "/home/pi/workspace/IoTOMATO/gateway/aws_iot/root-CA.crt"
-KEY_PATH   = "/home/pi/workspace/IoTOMATO/gateway/aws_iot/iotomato-device.private.key"
-CERT_PATH  = "/home/pi/workspace/IoTOMATO/gateway/aws_iot/iotomato-device.cert.pem"
+KEY_PATH   = "/home/pi/workspace/IoTOMATO/gateway/aws_iot/iotomato.private.key"
+CERT_PATH  = "/home/pi/workspace/IoTOMATO/gateway/aws_iot/iotomato.cert.pem"
 MAIN_TOPIC = "iotomato/"
 CLIENT_ID  = "IoTOMATO"
 PORT = '/dev/ttyUSB0'
 BAND_RATE = 9600
-DIRECTORY_NAME = "/home/pi/workspace/IoTOMATO/gateway/data/sensing_part/"
+DIRECTORY_NAME = "/home/pi/workspace/IoTOMATO/gateway/data/"
 
 
 def store_data(directory_name_str, file_name_str, payload_dit):
@@ -34,7 +34,7 @@ def store_data(directory_name_str, file_name_str, payload_dit):
         os.chdir(directory_name_str)
 
     except FileNotFoundError:
-        os.mkdir(directory_name_str)
+        os.makedirs(directory_name_str)
         os.chdir(directory_name_str)
 
     with open(file_name_str, 'a') as fp:
@@ -44,26 +44,14 @@ def store_data(directory_name_str, file_name_str, payload_dit):
     os.chdir(cpath)
 
 
-def send_to_mqtt_broker(sub_topic_str, data_dit):
-    device_str = sub_topic_str
-
-    payload_dit = {}
-    payload_dit['timestamp'] =  data_dit['timestamp']
-    payload_dit['device'] = device_str
-    payload_dit.update(data_dit[device_str])
-     
-    mqtt_client.publish(MAIN_TOPIC + sub_topic_str, json.dumps(payload_dit), 0)
+def send_to_mqtt_broker(sub_topic, data_dit):
+    topic = MAIN_TOPIC + sub_topic
+    mqtt_client.publish(topic, json.dumps(data_dit), 0)
 
 
-def send_to_aws_iot(sub_topic_str, data_dit):
-    device_str = sub_topic_str
-
-    payload_dit = {}
-    payload_dit['timestamp'] =  data_dit['timestamp']
-    payload_dit['device'] = device_str
-    payload_dit.update(data_dit[device_str])
-     
-    myAWSIoTMQTTClient.publish(MAIN_TOPIC + sub_topic_str, json.dumps(payload_dit), 1)
+def send_to_aws_iot(sub_topic, data_dit):
+    topic = MAIN_TOPIC + sub_topic
+    myAWSIoTMQTTClient.publish(topic, json.dumps(data_dit), 1)
 
 
 def handle_xbee(xbee_packet):
@@ -72,16 +60,17 @@ def handle_xbee(xbee_packet):
     file_name_str = timestamp_datetime.strftime("%Y-%m-%d") + ".json"
 
     try:
-        payload_dit = dict()
-        data_dit = ast.literal_eval((xbee_packet['rf_data']).decode('utf-8'))
-        payload_dit.update(data_dit)
-        sub_topic_str = list(payload_dit.keys())[0]
-        payload_dit.update({'timestamp' : timestamp_str})
-        directory_name_str = sub_topic_str
+        payload_dit = ast.literal_eval((xbee_packet['rf_data']).decode('utf-8'))
+        target = list(payload_dit.keys())[0]
+        data_dit = payload_dit[target]
+        device_id = data_dit['device_id']
+        data_dit.update({'timestamp' : timestamp_str})
+        sub_topic = 'device/' + target + '/device_id/' + device_id
+        directory_name_str = sub_topic
 
-        thread_send_to_aws_iot = threading.Thread(target=send_to_aws_iot, args=(sub_topic_str, payload_dit))
-        thread_send_to_mqtt_broker = threading.Thread(target=send_to_mqtt_broker, args=(sub_topic_str, payload_dit))
-        thread_store_data = threading.Thread(target=store_data, args=(directory_name_str, file_name_str, payload_dit))
+        thread_send_to_aws_iot = threading.Thread(target=send_to_aws_iot, args=(sub_topic, data_dit))
+        thread_send_to_mqtt_broker = threading.Thread(target=send_to_mqtt_broker, args=(sub_topic, payload_dit))
+        thread_store_data = threading.Thread(target=store_data, args=(directory_name_str, file_name_str, data_dit))
 
         thread_send_to_aws_iot.start()
         thread_send_to_mqtt_broker.start()
@@ -120,8 +109,7 @@ xbee = ZigBee(serial_port, escaped=True, callback=handle_xbee)
 
 
 try:
-    while True:
-        time.sleep(0.001)
+    mqtt_client.loop_forever()
 
 except KeyboardInterrupt:
     myAWSIoTMQTTClient.disconnect()
@@ -129,5 +117,3 @@ except KeyboardInterrupt:
     xbee.halt()
     serial_port.close()
     exit(0)
-
-
